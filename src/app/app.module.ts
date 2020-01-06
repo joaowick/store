9e9d5ad210e7d7655f09e1e60c39358284279581
Joao Torres <joaotorrez14@gmail.com>
2020-01-06 17:12:52 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'iPhone 11 Pro',
+    price: 4999,
+    description: 'Um dos melhores da atualidade'
+  },
+  {
+    name: 'Samsung Galaxy S20',
+    price: 3999,
+    description: 'Melhor do mundo'
+  },
+  {
+    name: 'Nokia Tijolão',
+    price: 199,
+    description: 'Melhor da história'
+  },
+  {
+    name: 'Fulano',
+    price: 1999,
+    description: ''
+  }
+]
